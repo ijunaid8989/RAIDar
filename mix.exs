9f3902a1eaed11a2ebcde87489d154453f343706
Junaid Farooq <ijunaidfarooq@gmail.com>
2018-01-27 07:01:52 +0500

    Added SSH to connect with servers.

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule ServerStatus.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:comeonin, "~> 3.0.2"}
+      {:comeonin, "~> 3.0.2"},
+      {:sshex, "2.2.1"}
     ]
   end
 
